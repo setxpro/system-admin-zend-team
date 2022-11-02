b18e1c253ae1b3882cb2ebe4505950b3a738b9df
Patrick Anjos <patrickpqdt87289@gmail.com>
2022-11-02 11:13:00 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { ToastContainer } from "react-toastify";
 import { RoutesApp } from "./Routes";
 import GlobalStyle from "./Styles/GlobalStyle";
 export const App = () => {
   return (
-    <>
+    <React.Fragment>
       <ToastContainer />
       <GlobalStyle />
       <RoutesApp />
-    </>
+    </React.Fragment>
   );
 };
